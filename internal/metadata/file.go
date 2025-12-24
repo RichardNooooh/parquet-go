1f2403c8093c6ef46f41aaa05c8c8198b203060a
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-23 18:31:25 -0600

    feat: Add footer metadata retrieval

--- internal/metadata/file.go
+++ internal/metadata/file.go
@@ -2,14 +2,16 @@ package metadata
 
 // "file" refers to the file metadata located at the footer of the Parquet file
 import (
-	// "github.com/apache/thrift/lib/go/thrift"
-	// "internal/gen-go/parquet"
 	"bytes"
+	"context"
 	"encoding/binary"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+
+	genparquet "github.com/RichardNooooh/parquet-go/internal/metadata/internal/gen-go/parquet"
+	thrift "github.com/apache/thrift/lib/go/thrift"
 )
 
 var PARQUET_MAGIC = []byte("PAR1")
@@ -17,7 +19,47 @@ var ErrorNotParquet = errors.New("not a Parquet file")
 
 const WORD_LENGTH = 4
 
-func processFooterMetadata(file *os.File) (uint32, error) {
+func ProcessFooterMetadata(file *os.File) error {
+	footerMetadataSize, err := getFooterMetadataSize(file)
+	if err != nil {
+		return err
+	}
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	compactMetadataBuffer := make([]byte, footerMetadataSize)
+	count, err := file.ReadAt(compactMetadataBuffer, fileStat.Size()-2*WORD_LENGTH-int64(footerMetadataSize))
+	if err != nil {
+		return fmt.Errorf("unable to read footer metadata: %w", err)
+	}
+	if count < int(footerMetadataSize) {
+		return fmt.Errorf("unable to read all footer metadata")
+	}
+	log.Printf("Retrieved footer metadata\n")
+
+	config := &thrift.TConfiguration{}
+	thriftBuffer := thrift.NewTMemoryBufferLen(int(footerMetadataSize))
+	_, err = thriftBuffer.Write(compactMetadataBuffer)
+	if err != nil {
+		return fmt.Errorf("failed to transfer footer metadata to thrift buffer: %w", err)
+	}
+
+	protocolConfig := thrift.NewTCompactProtocolConf(thriftBuffer, config)
+	metadata := genparquet.NewFileMetaData()
+	err = metadata.Read(context.Background(), protocolConfig)
+	if err != nil {
+		return fmt.Errorf("failed to decode thrift metadata: %w", err)
+	}
+
+	log.Printf("Processed Thrift Metadata Structure: %v\n", metadata)
+
+	return nil
+}
+
+func getFooterMetadataSize(file *os.File) (uint32, error) {
 	var footerLengthBuffer [WORD_LENGTH]byte
 
 	fileStat, err := file.Stat()
@@ -89,6 +131,9 @@ func Start(fileName string) error {
 	if err != nil {
 		return fmt.Errorf("%v had Parquet check errors: %w", fileName, err)
 	}
-	processFooterMetadata(file)
+	err = ProcessFooterMetadata(file)
+	if err != nil {
+		return err
+	}
 	return nil
 }
