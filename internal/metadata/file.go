f2573bc03091b1707036aed10ad52151b37e7d14
RichardNooooh <richard.jun.noh@gmail.com>
2025-12-24 03:43:54 -0600

    refactor: Clean API for file metadata processing
    refactor: Move internal parquet thrift package

--- internal/metadata/file.go
+++ internal/metadata/file.go
@@ -7,95 +7,91 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"log"
-	"os"
 
-	genparquet "github.com/RichardNooooh/parquet-go/internal/metadata/internal/gen-go/parquet"
+	genparquet "github.com/RichardNooooh/parquet-go/internal/metadata/gen-go/parquet"
 	thrift "github.com/apache/thrift/lib/go/thrift"
 )
 
 var PARQUET_MAGIC = []byte("PAR1")
-var ErrorNotParquet = errors.New("not a Parquet file")
+var ErrNotParquet = errors.New("not a Parquet file")
 
-const WORD_LENGTH = 4
+const WORD_LENGTH int64 = 4
 
-func ProcessFooterMetadata(file *os.File) error {
-	footerMetadataSize, err := getFooterMetadataSize(file)
+type FileReader struct {
+	reader io.ReaderAt
+	size   int64
+}
+
+func GetFileMetadata(ctx context.Context, file *FileReader) (*genparquet.FileMetaData, error) {
+	if err := checkParquet(file); err != nil {
+		return nil, err
+	}
+	fileMetadataSize, err := getFileMetadataSize(file)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fileStat, err := file.Stat()
+	compactMetadataBuffer := make([]byte, fileMetadataSize)
+	count, err := file.reader.ReadAt(compactMetadataBuffer, file.size-2*WORD_LENGTH-int64(fileMetadataSize))
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("unable to read footer metadata: %w", err)
 	}
-
-	compactMetadataBuffer := make([]byte, footerMetadataSize)
-	count, err := file.ReadAt(compactMetadataBuffer, fileStat.Size()-2*WORD_LENGTH-int64(footerMetadataSize))
-	if err != nil {
-		return fmt.Errorf("unable to read footer metadata: %w", err)
-	}
-	if count < int(footerMetadataSize) {
-		return fmt.Errorf("unable to read all footer metadata")
+	if count < int(fileMetadataSize) {
+		return nil, fmt.Errorf("unable to read all footer metadata")
 	}
 	log.Printf("Retrieved footer metadata\n")
 
 	config := &thrift.TConfiguration{}
-	thriftBuffer := thrift.NewTMemoryBufferLen(int(footerMetadataSize))
+	thriftBuffer := thrift.NewTMemoryBufferLen(int(fileMetadataSize))
 	_, err = thriftBuffer.Write(compactMetadataBuffer)
 	if err != nil {
-		return fmt.Errorf("failed to transfer footer metadata to thrift buffer: %w", err)
+		return nil, fmt.Errorf("failed to transfer footer metadata to thrift buffer: %w", err)
 	}
 
 	protocolConfig := thrift.NewTCompactProtocolConf(thriftBuffer, config)
-	metadata := genparquet.NewFileMetaData()
-	err = metadata.Read(context.Background(), protocolConfig)
+	fileMetadata := genparquet.NewFileMetaData()
+	err = fileMetadata.Read(ctx, protocolConfig)
 	if err != nil {
-		return fmt.Errorf("failed to decode thrift metadata: %w", err)
+		return nil, fmt.Errorf("failed to decode thrift metadata: %w", err)
 	}
 
-	log.Printf("Processed Thrift Metadata Structure: %v\n", metadata)
-
-	return nil
+	return fileMetadata, nil
 }
 
-func getFooterMetadataSize(file *os.File) (uint32, error) {
-	var footerLengthBuffer [WORD_LENGTH]byte
+func getFileMetadataSize(file *FileReader) (uint32, error) {
+	var fileMetadataLengthBuffer [WORD_LENGTH]byte
 
-	fileStat, err := file.Stat()
+	count, err := file.reader.ReadAt(fileMetadataLengthBuffer[:], int64(file.size-(2*WORD_LENGTH)))
 	if err != nil {
-		return 0, fmt.Errorf("stat file error: %w", err)
+		return 0, fmt.Errorf("%w: missing file metadata size", ErrNotParquet)
+	}
+	if int64(count) < WORD_LENGTH {
+		return 0, fmt.Errorf("%w: could not read enough bytes for file metadata size", ErrNotParquet)
 	}
 
-	fileSize := fileStat.Size()
-	count, err := file.ReadAt(footerLengthBuffer[:], fileSize-(2*WORD_LENGTH))
-	if err != nil {
-		return 0, fmt.Errorf("%w: missing file metadata size", ErrorNotParquet)
-	}
-	if count < WORD_LENGTH {
-		return 0, fmt.Errorf("%w: could not read enough bytes for file metadata size", ErrorNotParquet)
+	fileMetadataSize := binary.LittleEndian.Uint32(fileMetadataLengthBuffer[:])
+	if int64(fileMetadataSize) > file.size-2*WORD_LENGTH {
+		return 0, fmt.Errorf("%w: file metadata too large (%d bytes)", ErrNotParquet, fileMetadataSize)
+	} else if fileMetadataSize == 0 {
+		return 0, fmt.Errorf("%w: file metadata is of size 0", ErrNotParquet)
 	}
 
-	footerMetadataSize := binary.LittleEndian.Uint32(footerLengthBuffer[:])
-	log.Printf("file metadata size: %d\n", footerMetadataSize)
+	log.Printf("file metadata size: %d\n", fileMetadataSize)
 
-	return footerMetadataSize, nil
+	return fileMetadataSize, nil
 }
 
-func checkParquet(file *os.File) error {
-	fileStat, err := file.Stat()
-	if err != nil {
-		return fmt.Errorf("stat file error: %w", err)
-	}
-
-	size := fileStat.Size()
+func checkParquet(file *FileReader) error {
+	size := file.size
 	if size < 3*WORD_LENGTH {
-		return fmt.Errorf("%w: file is too small! minimum size: %d, actual size: %d", ErrorNotParquet, 3*WORD_LENGTH, size)
+		return fmt.Errorf("%w: file is too small! minimum size: %d, actual size: %d", ErrNotParquet, 3*WORD_LENGTH, size)
 	}
 
 	var buffer [WORD_LENGTH]byte
 
-	_, err = file.ReadAt(buffer[:], 0)
+	_, err := file.reader.ReadAt(buffer[:], 0)
 	if err != nil {
 		return fmt.Errorf("could not read enough bytes at start of file: %w", err)
 	}
@@ -103,10 +99,10 @@ func checkParquet(file *os.File) error {
 	// check first 4
 	hasHeaderMagic := bytes.Equal(buffer[:], PARQUET_MAGIC)
 	if !hasHeaderMagic {
-		return fmt.Errorf("%w: header magic mismatch: got %q", ErrorNotParquet, buffer[:])
+		return fmt.Errorf("%w: header magic mismatch: got %q", ErrNotParquet, buffer[:])
 	}
 
-	_, err = file.ReadAt(buffer[:], size-int64(WORD_LENGTH))
+	_, err = file.reader.ReadAt(buffer[:], int64(size-WORD_LENGTH))
 	if err != nil {
 		return fmt.Errorf("could not read enough bytes at end of file: %w", err)
 	}
@@ -114,26 +110,32 @@ func checkParquet(file *os.File) error {
 	// check last 4
 	hasFooterMagic := bytes.Equal(buffer[:], PARQUET_MAGIC)
 	if !hasFooterMagic {
-		return fmt.Errorf("%w: footer magic mismatch: got %q", ErrorNotParquet, buffer[:])
+		return fmt.Errorf("%w: footer magic mismatch: got %q", ErrNotParquet, buffer[:])
 	}
 
 	return nil
 }
 
-func Start(fileName string) error {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return fmt.Errorf("no such file exists: %w", err)
-	}
-	defer file.Close()
-
-	err = checkParquet(file)
-	if err != nil {
-		return fmt.Errorf("%v had Parquet check errors: %w", fileName, err)
-	}
-	err = ProcessFooterMetadata(file)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func Start(fileName string) error {
+// 	file, err := os.Open(fileName)
+// 	if err != nil {
+// 		return fmt.Errorf("no such file exists: %w", err)
+// 	}
+// 	defer file.Close()
+//
+// 	fileStat, err := file.Stat()
+// 	if err != nil {
+// 		return fmt.Errorf("could not read file size: %w", err)
+// 	}
+//
+// 	fileReader := &FileReader{file, fileStat.Size()}
+// 	err = checkParquet(fileReader)
+// 	if err != nil {
+// 		return fmt.Errorf("%v had Parquet check errors: %w", fileName, err)
+// 	}
+// 	_, err = ProcessFileMetadata(fileReader)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
